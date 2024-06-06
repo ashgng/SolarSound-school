cfba2be3b50f70a781d3a62c8ffe73fa65f322a6
ashgng <marceau.giang@insa-lyon.fr>
2024-06-06 22:26:37 +0200

    ajustements graphiques

--- game.py
+++ game.py
@@ -241,7 +241,10 @@ def run_loading_screen():
         
         screen.blit(background_image, (0, 0))
         pygame.draw.rect(screen, WHITE, (x_bar, y_bar, bar_width, bar_height))
+        pygame.draw.rect(screen, BLACK, (x_bar, y_bar, bar_width, bar_height),2)
         pygame.draw.rect(screen, PURPLE, (x_bar, y_bar, fill_width, bar_height))
+        pygame.draw.rect(screen, BLACK, (x_bar, y_bar, fill_width, bar_height),2)
+        
 
         # Display current step text
         if current_step < len(step_texts):
